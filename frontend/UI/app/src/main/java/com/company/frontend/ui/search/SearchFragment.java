da96dbea360eba70cbcd69a7fa429beed116217f
ZiyuWangggg <ziyu4@illinois.edu>
2022-10-06 14:47:37 -0500

    resolve merge issue

--- frontend/UI/app/src/main/java/com/company/frontend/ui/search/SearchFragment.java
+++ frontend/UI/app/src/main/java/com/company/frontend/ui/search/SearchFragment.java
@@ -4,25 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.company.frontend.databinding.FragmentHomeBinding;
 
-public class HomeFragment extends Fragment {
+public class SearchFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    private SearchViewModel homeViewModel;
     private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+                new ViewModelProvider(this).get(SearchViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
