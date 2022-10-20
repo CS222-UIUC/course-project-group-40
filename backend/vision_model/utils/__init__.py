d72032eda6fd79cc51f5fa68252fc5fa4704e908
Ewan Yuanxi Li <79686539+yuanxili1@users.noreply.github.com>
2022-10-20 17:10:12 -0500

    Add vision-model Utils & Configuration

---
+++ backend/vision_model/utils/__init__.py
@@ -0,0 +1,6 @@
+from .logging import get_logger # noqa
+from .init import weight_init # noqa
+from .label_convert import CTCLabelConverter # noqa
+from .ckpt import load_checkpoint, save_checkpoint, save_checkpoint_logic # noqa
+from .vis import get_font_file, draw_ocr_box_txt, show_img, draw_bbox # noqa
+# from .save import save_checkpoint
