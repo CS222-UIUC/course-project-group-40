7adf4f64f22bfef8168b2d184a44f2598b92f077
Ewan Yuanxi Li <79686539+yuanxili1@users.noreply.github.com>
2022-11-10 17:54:51 -0600

    Add Obj Detection Module

--- backend/vision_model/test/test_predict.py
+++ backend/vision_model/test/test_predict.py
@@ -42,24 +42,28 @@ def test_RecInfer():
 
 def test_ObjDecInfer():
     from src.predict import ObjDecInfer, init_args
-    # import cv2
+    import cv2
 
     sys.argv = [
         '',
         '--model_path',
-        './output/ocr/CRNN/ch_rec_moblie_crnn_mbv.pth',
+        './output/object',
         '--img_path',
-        './dataset/ocr/icdar2015/recognition/test/img_10_1.jpg'
+        './dataset/object/samples/dog.jpg'
     ]
     args = init_args()
 
     model = ObjDecInfer(args.model_path)
-    # img_list = [cv2.imread(args.img_path)]
-    # out_list = model.predict(img_list)
-
-    # img = cv2.imread(args.img_path)
-    # out = model.predict(img)
+    img_list = [cv2.imread(args.img_path)]
+    out_list = model.predict(img_list)
+    print(out_list)
+    img = cv2.imread(args.img_path)
+    out = model.predict(img)
 
     # TESTS
     assert model.batch_size == 16
     assert model.device == torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    assert out_list[0][4] == 'bicycle'
+    assert out_list[1][4] == 'dog'
+    assert out_list[2][4] == 'truck'
+    assert out[0][4] == 'bicycle'
--- backend/vision_model/utils/__init__.py
+++ backend/vision_model/utils/__init__.py
@@ -3,4 +3,4 @@ from .init import weight_init # noqa
 from .label_convert import CTCLabelConverter # noqa
 from .ckpt import load_checkpoint, save_checkpoint, save_checkpoint_logic # noqa
 from .vis import get_font_file, draw_ocr_box_txt, show_img, draw_bbox # noqa
-# from .save import save_checkpoint
+from .save import save_checkpoint # noqa
