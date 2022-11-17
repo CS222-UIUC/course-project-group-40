7821404cd02f5ad92b050b5c8fa052e503c763d7
ZiyuWangggg <ziyu4@illinois.edu>
2022-11-17 15:28:25 -0600

    add activity page for OCR

---
+++ frontend/android-frontend/app/src/main/java/com/company/android_frontend/OCRActivity.java
@@ -0,0 +1,27 @@
+package com.company.android_frontend;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OCRActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        Intent intent = getIntent();
+        String value = intent.getStringExtra("textRecognized");
+        if (value == null || value.isEmpty()) {
+            value = "Default text here";
+        }
+
+        TextView resultView = findViewById(R.id.resultRecognized);
+        resultView.setText(value);
+        resultView.setMovementMethod(new ScrollingMovementMethod());
+
+    }
+}
